9e5ddbe56188ff93dabeb675a13c71329319bfe6
khanyong <khanyong.yoo@gmail.com>
2025-07-24 22:15:32 +0900

    CRON_SECRET 인증 체크 추가

--- pages/api/trigger-update-videos.js
+++ pages/api/trigger-update-videos.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  // CRON_SECRET 체크
+  if (req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).json({ code: 401, message: "Missing authorization header" });
+  }
+
   const response = await fetch(
     "https://iklsghevdtqqkjuaympc.supabase.co/functions/v1/update-videos",
     { method: "POST" }
